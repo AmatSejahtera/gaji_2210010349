995e9d81e8e36e09b3565b517b1b8e96d02443f6
Muhammad Rif'at Kiron <gokiliat321@gmail.com>
2026-01-12 21:01:59 +0800

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['prefix' => 'admin', 'middleware' => ['auth']], function () {
+    Route::resource('pengguna', UserController::class);
+});
