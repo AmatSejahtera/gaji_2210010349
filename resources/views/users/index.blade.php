dcfe7085efdc40ccfe1cb786fb88bb08e81040de
Muhammad Rif'at Kiron <gokiliat321@gmail.com>
2026-01-13 20:44:40 +0800

    Update index.blade.php

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -16,38 +16,66 @@
                 </div>
                 <div class="card-body">
                     <div class="table-responsive">
-                        <table class="table table-bordered">
-                            <tr>
-                                <th>NO.</th>
-                                <th>NAMA LENGKAP</th>
-                                <th>EMAIL</th>
-                                <th>LEVEL</th>
-                                <th class="text-center">AKSI</th>
-                            </tr>
-                            @php
-                                $no = 1;
-                            @endphp
-                            @foreach ($users as $user)
+                        <table id="usersTable" class="table table-bordered table-striped table-hover">
+                            <thead>
                                 <tr>
-                                    <td>{{ $no++ }}</td>
-                                    <td>{{ $user->name }}</td>
-                                    <td>{{ $user->email }}</td>
-                                    <td>{{ $user->level }}</td>
-                                    <td class="text-center">
-                                        <form action="{{ route('pengguna.destroy', $user->id) }}" method="post">
-                                            @method('DELETE')
-                                            @csrf
-                                            <a href="{{ route('pengguna.edit', $user->id) }}"
-                                                class="btn btn-md btn-warning">EDIT</a>
-                                            <button type="submit" class="btn btn-md btn-danger">HAPUS</button>
-                                        </form>
-                                    </td>
+                                    <th>NO.</th>
+                                    <th>NAMA LENGKAP</th>
+                                    <th>EMAIL</th>
+                                    <th>LEVEL</th>
+                                    <th class="text-center">AKSI</th>
                                 </tr>
-                            @endforeach
+                            </thead>
+                            <tbody>
+                                @php
+                                    $no = 1;
+                                @endphp
+                                @foreach ($users as $user)
+                                    <tr>
+                                        <td>{{ $no++ }}</td>
+                                        <td>{{ $user->name }}</td>
+                                        <td>{{ $user->email }}</td>
+                                        <td>{{ $user->level }}</td>
+                                        <td class="text-center">
+                                            <form action="{{ route('pengguna.destroy', $user->id) }}" method="post">
+                                                @method('DELETE')
+                                                @csrf
+                                                <a href="{{ route('pengguna.edit', $user->id) }}"
+                                                    class="btn btn-md btn-warning">EDIT</a>
+                                                <button type="submit" class="btn btn-md btn-danger">HAPUS</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
                         </table>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+@stop
+
+@section('adminlte_js')
+    <script>
+        $(document).ready(function() {
+            $('#usersTable').DataTable({
+                "responsive": true,
+                "lengthChange": true,
+                "autoWidth": false,
+                "pageLength": 10,
+                "lengthMenu": [[10, 25, 50, 100], [10, 25, 50, 100]],
+                "order": [[0, 'asc']],
+                "language": {
+                    "url": "//cdn.datatables.net/plug-ins/1.10.19/i18n/Indonesian.json"
+                },
+                "columnDefs": [
+                    {
+                        "orderable": false,
+                        "targets": 4
+                    }
+                ]
+            });
+        });
+    </script>
 @stop
